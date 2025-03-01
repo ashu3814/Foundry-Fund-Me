e6a2cded946802f080905640b5462997ed10386e
SHAIK ASHFAQ <ashfaq@chingari.io>
2025-03-01 14:51:42 +0530

    testing the code using Foundry

---
+++ test/FundMeTest.t.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.18;
+
+import {Test , console} from "forge-std/Test.sol";
+import {FundMe} from "../src/FundMe.sol";
+
+contract FundMeTest is Test {
+    FundMe fundMe;
+    function setUp() external {
+     fundMe = new FundMe();
+    }
+
+    function testMinimumDollarIsFive() external view {
+        assert(fundMe.MINIMUM_USD() == 5e18);
+    }
+
+}
